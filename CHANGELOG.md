3164600500c880b81fc75a6e6e2892c0347cf056
t7yang <t7yang@gmail.com>
2018-03-12 08:29:23 +0800

    update 1.0.1

---
+++ CHANGELOG.md
@@ -0,0 +1,11 @@
+# TongWen Core Changelog
+
+## 1.0.1
+* DEPRECATED:
+  * Prepare for npm publish
+    * Install via git repo not longer provide postInstall process, you should do it your own
+    * Move default `dictionaries` folder from `./src/` to `./`
+    * Add `CHANGELOG.md`
+
+## 1.0.0
+* first release
--- src/tongwen-core/index.test.ts
+++ src/tongwen-core/index.test.ts
@@ -1,10 +1,10 @@
 import { TongWenCore } from './';
 import { Core_RawGroupDic } from './interface';
 
-const defaultPS2TTable = require('../dictionaries/s2t_phrase.json');
-const defaultPT2STable = require('../dictionaries/t2s_phrase.json');
-const defaultS2TTable = require('../dictionaries/s2t_char.json');
-const defaultT2STable = require('../dictionaries/t2s_char.json');
+const defaultPS2TTable = require('../../dictionaries/s2t_phrase.json');
+const defaultPT2STable = require('../../dictionaries/t2s_phrase.json');
+const defaultS2TTable = require('../../dictionaries/s2t_char.json');
+const defaultT2STable = require('../../dictionaries/t2s_char.json');
 
 const DICTIONARY2: Core_RawGroupDic = {
   s2t: {
