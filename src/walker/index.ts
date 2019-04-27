fa6269afba3900941ed85812884e6e5a5e2a1830
t7yang <t7yang@gmail.com>
2019-04-28 03:46:46 +0800

    feat: export helper function in walker

--- src/walker/index.ts
+++ src/walker/index.ts
@@ -1,3 +1,4 @@
 export * from './async-walker';
+export * from './shared';
 export * from './types';
 export * from './walker';
---
+++ src/walker/shared/index.ts
@@ -0,0 +1,7 @@
+export * from './accept-node';
+export * from './constant';
+export * from './extract-attr-text';
+export * from './has-chinese';
+export * from './has-target-attr';
+export * from './has-target-attrs';
+export * from './parse-node';
