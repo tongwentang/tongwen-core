2014b147348ea5cd1f00a2747da7d881fd22592a
t7yang <t7yang@gmail.com>
2021-04-23 15:59:53 +0800

    test: Update `walkNode` test to test text node in div.

--- src/walker/walk.test.ts
+++ src/walker/walk.test.ts
@@ -12,8 +12,8 @@ describe('test walkNode', () => {
       <!DOCTYPE html>
       <body>
         <div>
+          “？”
           <span>I 愛 U</span>
-          <span>“Triangulum”</span>
           <span>Should not extract none target string!!</span>
           <div id="editable" contenteditable="true" aria-label="可編輯的 attribute">可編輯的內容</div>
           <a href="https://www.example.com" title="首頁"></a>
@@ -25,14 +25,15 @@ describe('test walkNode', () => {
     spy.mockImplementation(() => dom.window.document);
 
     const parseds = walkNode(dom.window.document.body);
-    const [span1, span2] = Array.from(dom.window.document.querySelectorAll('span'));
+    const div = dom.window.document.querySelector('div')!;
+    const [span1] = Array.from(dom.window.document.querySelectorAll('span'));
     const editable = dom.window.document.querySelector('div#editable')!;
     const a = dom.window.document.querySelector('a')!;
     const expected: ParsedResult[] = [
+      { type: 'TEXT', node: div.childNodes[0], text: div.childNodes[0].nodeValue! },
       { type: 'TEXT', node: span1.childNodes[0], text: 'I 愛 U' },
-      { type: 'TEXT', node: span2.childNodes[0], text: '“Triangulum”' },
-      { type: 'ELEMENT', node: editable, text: '可編輯的 attribute' },
-      { type: 'ELEMENT', node: a, text: '首頁' },
+      { type: 'ELEMENT', node: editable, attr: 'aria-label', text: '可編輯的 attribute' },
+      { type: 'ELEMENT', node: a, attr: 'title', text: '首頁' },
     ];
 
     expect(parseds).toEqual(expected);
