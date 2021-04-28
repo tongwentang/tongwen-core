580dc3a87ec4965557a0abe49b6504a96a8e44bb
t7yang <t7yang@gmail.com>
2021-04-28 16:42:28 +0800

    test: Update `walkNode` test for `isContentEditable` check.

--- src/walker/walk.test.ts
+++ src/walker/walk.test.ts
@@ -24,17 +24,39 @@ describe('test walkNode', () => {
     const spy = jest.spyOn(global, 'document', 'get');
     spy.mockImplementation(() => dom.window.document);
 
-    const parseds = walkNode(dom.window.document.body);
     const div = dom.window.document.querySelector('div')!;
     const [span1] = Array.from(dom.window.document.querySelectorAll('span'));
     const editable = dom.window.document.querySelector('div#editable')!;
+    (editable as any).isContentEditable = true; // JSDom does not support isContentEditable yet
     const a = dom.window.document.querySelector('a')!;
+    const parseds = walkNode(dom.window.document.body);
     const expected: ParsedResult[] = [
       { type: 'TEXT', node: div.childNodes[0], text: div.childNodes[0].nodeValue! },
       { type: 'TEXT', node: span1.childNodes[0], text: 'I 愛 U' },
       { type: 'ELEMENT', node: editable, attr: 'aria-label', text: '可編輯的 attribute' },
       { type: 'ELEMENT', node: a, attr: 'title', text: '首頁' },
     ];
+
+    expect(parseds).toEqual(expected);
+  });
+
+  it('should extract attributes only for editable element', () => {
+    const dom = new JSDOM(`
+      <!DOCTYPE html>
+      <body>
+        <div id="editable" contenteditable="true">
+          <a href="https://www.example.com" title="首頁">內部文字</a>
+        </div>
+      </body>
+    `);
+
+    const spy = jest.spyOn(global, 'document', 'get');
+    spy.mockImplementation(() => dom.window.document);
+    const node = dom.window.document.querySelector('div#editable')!;
+    const a = node.children[0] as HTMLAnchorElement;
+    (a as any).isContentEditable = true; // JSDom does not support isContentEditable yet
+    const parseds = walkNode(a);
+    const expected: ParsedResult[] = [{ type: 'ELEMENT', node: a, attr: 'title', text: '首頁' }];
 
     expect(parseds).toEqual(expected);
   });
