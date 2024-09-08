a6010e6622a63be440dd51b854e1541fe7f55345
t7yang <t7yang@gmail.com>
2024-09-09 02:40:12 +0800

    build: added cjs support

--- tsup.config.js
+++ tsup.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'tsup';
 
 export default defineConfig({
   entry: ['src/converter/index.ts', 'src/dictionary/index.ts', 'src/walker/index.ts'],
-  format: ['esm'],
+  format: ['esm', 'cjs'],
   dts: true,
   target: 'node20',
   sourcemap: true,
