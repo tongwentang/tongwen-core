9203fd9c6fb868edf670ce2303236b97000a5a90
RossWang <RossWang@users.noreply.github.com>
2025-04-25 23:11:32 +0800

    feat: option to keep length of output
    minimum changes to accommodate the requirement of extension

--- src/converter/types.ts
+++ src/converter/types.ts
@@ -2,7 +2,7 @@ import { LangType, type SrcPack } from '../dictionary/type.js';
 
 export type UpdateSource = (src: SrcPack) => void;
 
-export type ConvertText = (type: LangType, text: string) => string;
+export type ConvertText = (type: LangType, text: string, fixedLen?: boolean) => string;
 
 export type Converter = { set: UpdateSource; char: ConvertText; phrase: ConvertText };
 
